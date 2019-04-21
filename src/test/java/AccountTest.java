c2c1bc567bc9f13a8fdb8ce8aa4d81356720da2d
Ryan Cheong <lastboy0@gmail.com>
2019-04-22 00:49:57 +0900

    assertThat 으로 잔액을 확인하는 코드 추가

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class AccountTest {
 
     @Test
@@ -7,6 +10,7 @@ public class AccountTest {
         Account account = new Account();
 
         int balance = account.getBalance();
+        assertThat(balance, is(0));
 
     }
 }
